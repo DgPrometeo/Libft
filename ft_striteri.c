a8695957e66bfe1b0e567db6357c1a37f09e54bb
Daniel Garcia Sanchez <danielga@c2r5s2.42malaga.com>
2022-12-19 14:19:23 +0100

    Striteri correct

---
+++ ft_striteri.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danielga <danielga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 12:46:47 by danielga          #+#    #+#             */
+/*   Updated: 2022/12/19 13:14:12 by danielga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+	size_t			len;
+
+	len = ft_strlen(s);
+	i = 0;
+	while (i < len)
+	{
+		f(i, &s[i]);
+		i++;
+	}
+	s[i] = '\0';
+}
